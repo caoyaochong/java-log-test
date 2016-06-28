576157518bbfcda8ecbe42518bd32c71fc170b54
caoyc <caoyc@getui.com>
2016-06-28 09:52:53 +0800

    初始化项目

---
+++ src/main/java/cn/edu/zju/cst/example/java_log_test/App.java
@@ -0,0 +1,13 @@
+package cn.edu.zju.cst.example.java_log_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
