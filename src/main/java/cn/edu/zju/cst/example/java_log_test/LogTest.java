4a0d003babfea450955f2a51096b2c2026b33a44
caoyc <caoyc@getui.com>
2016-06-30 16:38:53 +0800

    test git, add "hello log"

--- src/main/java/cn/edu/zju/cst/example/java_log_test/LogTest.java
+++ src/main/java/cn/edu/zju/cst/example/java_log_test/LogTest.java
@@ -3,7 +3,7 @@ package cn.edu.zju.cst.example.java_log_test;
 public class LogTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hello log");
 
 	}
 
